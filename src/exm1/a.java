a66990f46de90295ed218d39ceeb3438043c50f6
H <H@DESKTOP-H1OKE5I>
2019-02-10 01:45:44 +0200

    f exp

---
+++ src/exm1/a.java
@@ -0,0 +1,17 @@
+package exm1;
+
+import java.util.Scanner;
+public class a{
+public static void main(String [] args){
+	Scanner input = new Scanner (System.in);
+	int number1;
+	int number2;
+	int sum;
+	System.out.println("Enter first integer: ");
+	number1 = input.nextInt();
+	System.out.println("Enter second integer: ");
+	number2 = input.nextInt();
+	sum = number1 + number2;
+	System.out.printf("Sum is %d \n ",sum);
+}
+}
